91949fdafb5d63e8e3a986153a62f0b0bde21a8b
Derek Guenther <dguenther9@gmail.com>
2014-12-06 13:37:18 -0600

    Add grimetime 16 by 9 game overlay

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,11 @@ router.get('/overlays/grimetime/game4by3', function(req, res) {
   res.render('overlays/grimetime/game4by3', { title: 'Overlay', layout: false });
 });
 
+/* GET home page. */
+router.get('/overlays/grimetime/game16by9', function(req, res) {
+  res.render('overlays/grimetime/game16by9', { title: 'Overlay', layout: false });
+});
+
 /* GET commentators. */
 router.get('/overlays/commentators', function(req, res) {
   res.render('overlays/commentators', { title: 'Overlay', layout: false });
