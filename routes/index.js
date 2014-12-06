21eae893fa1690db62588bf30c7ebcf0c82733b2
Derek Guenther <dguenther9@gmail.com>
2014-12-06 11:55:38 -0600

    Start reorganizing folders, adding commentators overlay as well

--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,23 @@ router.get('/overlay', function(req, res) {
 });
 
 /* GET home page. */
-router.get('/overlay/admin', function(req, res) {
-  res.render('overlay/admin', { title: 'Overlay Admin' });
+router.get('/overlays/admin', function(req, res) {
+  res.render('overlays/admin', { title: 'Overlay Admin' });
 });
 
 /* GET home page. */
-router.get('/overlay/scores', function(req, res) {
-  res.render('overlay/scores', { title: 'Scores' });
+router.get('/overlays/scores', function(req, res) {
+  res.render('overlays/scores', { title: 'Scores' });
+});
+
+/* GET home page. */
+router.get('/overlays/grimetime/game4by3', function(req, res) {
+  res.render('overlays/grimetime/game4by3', { title: 'Overlay', layout: false });
+});
+
+/* GET commentators. */
+router.get('/overlays/commentators', function(req, res) {
+  res.render('overlays/commentators', { title: 'Overlay', layout: false });
 });
 
 module.exports = router;
