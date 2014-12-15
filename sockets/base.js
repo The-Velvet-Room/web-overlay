ecdea72c7fb1d86c745b3258f9378b35f335920a
Cameron Crockrom <ccrockrom@gmail.com>
2014-12-14 20:52:24 -0600

    Initial support for Twitch API functions.

--- sockets/base.js
+++ sockets/base.js
@@ -1,4 +1,6 @@
 var config = require('../config');
+//npm install xmlhttprequest if this fails
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 module.exports = function (io) {
 
   var data = {};
@@ -18,14 +20,64 @@ module.exports = function (io) {
     socket.on('update overlay', function(msg){
       data = msg;
       data.config = config;
-    	io.emit('update overlay', data);
-    	console.log('update overlay: ' + JSON.stringify(data));
+      if (data.twitchUsername)
+      {
+      	if(!data.twitchGame)
+      	{
+      		data = initializeTwitchData(data);
+      	}
+      	else
+      	{
+      		//data = updateTwitchData(data);
+      	}
+      }
+      	
+      io.emit('update overlay', data);
+      console.log('update overlay: ' + JSON.stringify(data));
   	});
 
     socket.on('request overlay', function() {
       console.log('fetching old overlay: ' + JSON.stringify(data));
       socket.emit('update overlay', data);
     });
+
+    function updateTwitchData(data) {
+    	var channelName = data.twitchUsername;
+    	var game = setTwitchGame(data.twitchUsername, data.twitchGame).game;
+    	var status = set
+    };
+
+    function initializeTwitchData(data) {
+    	var requestUrl = config.twitchApiRoot + '/channels/'+data.twitchUsername;
+    	var xmlhttp = new XMLHttpRequest();
+    	xmlhttp.open('GET',requestUrl,false);
+		xmlhttp.setRequestHeader('Accept','application/vnd.twitchtv.v2+json');
+		xmlhttp.setRequestHeader('Client-Id', config.twitchClientId);
+		xmlhttp.setRequestHeader('Authorization', 'OAuth '+config.twitchAccessToken);
+		xmlhttp.send();
+		console.log('Requesting channel data for '+data.twitchUsername);
+		var twitchResponse = JSON.parse(xmlhttp.responseText);
+		//console.log('Response: %j', twitchResponse);
+		data.twitchGame = twitchResponse.game;
+		data.twitchStatus = twitchResponse.status;
+		return data;
+    }
+
+    function setTwitchGame(channel, gameName) {
+    	var data = {"channel": {"game": game}}
+    	var stringQuery = JSON.stringify(data)
+    	var contentLength = stringQuery.length
+    	var requestUrl = config.twitchApiRoot + '/channels/'+channel+'?channel[game]='+game;
+    	
+    	xmlhttp.open('POST',requestUrl,false);
+		xmlhttp.setRequestHeader('Accept','application/vnd.twitchtv.v2+json');
+		xmlhttp.setRequestHeader('Client-Id', config.twitchClientId);
+		xmlhttp.setRequestHeader('Authorization', 'OAuth '+config.twitchAccessToken);
+		xmlhttp.setRequestHeader('Scope', 'channel-editor');
+		xmlhttp.setRequestHeader('Content-Length', contentLength);
+		xmlhttp.send(stringQuery);
+		return JSON.parse(xmlhttp.responseText);
+    }
   });
 
 }
