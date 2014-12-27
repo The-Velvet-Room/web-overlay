efa037defc3b4628a87e3a2e991933c5071a9794
Derek Guenther <dguenther9@gmail.com>
2014-12-27 15:01:07 -0600

    Remove pts suffix since we're namespaced now

--- sockets/pardonthesmash.js
+++ sockets/pardonthesmash.js
@@ -10,23 +10,23 @@ module.exports = function (io) {
     console.log('pardonthesmash user connected: ' + socket.handshake.address + ' -> ' + socket.request.headers.referer);
 
     // Send out existing data to the new connection
-    socket.emit('start timer pts', ptsData);
-    socket.emit('update topics pts', ptsTopics);
+    socket.emit('start timer', ptsData);
+    socket.emit('update topics', ptsTopics);
 
     socket.on('disconnect', function() {
       console.log('pardonthesmash user disconnected: ' + socket.handshake.address);
     });
 
-    socket.on('start timer pts', function(msg){
+    socket.on('start timer', function(msg){
       ptsData = msg;
-      pts.emit('start timer pts', ptsData);
-      console.log('start timer pts: ' + JSON.stringify(ptsData));
+      pts.emit('start timer', ptsData);
+      console.log('start timer: ' + JSON.stringify(ptsData));
     });
 
-    socket.on('update topics pts', function (msg) {
+    socket.on('update topics', function (msg) {
       ptsTopics = msg;
-      console.log('update topics pts: ' + JSON.stringify(ptsTopics));
-      pts.emit('update topics pts', ptsTopics);
+      console.log('update topics: ' + JSON.stringify(ptsTopics));
+      pts.emit('update topics', ptsTopics);
     });
   });
 
