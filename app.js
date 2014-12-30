54a0c5f850e8a89da093432fa47ae7c32e6a159a
Derek Guenther <dguenther9@gmail.com>
2014-12-29 23:32:34 -0600

    Added finishing styles to guest page

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
+var hbs = require('hbs');
+
+hbs.registerHelper('section', function(name, options) {
+    if(!this._sections) this._sections = {};
+    this._sections[name] = options.fn(this);
+    return null;
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
