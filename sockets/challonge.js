4a5bf88a54980be245c6329842d25447a5c7cade
Cameron Crockrom <ccrockrom@gmail.com>
2015-01-03 11:44:46 -0600

    CRs and refactor.

--- sockets/challonge.js
+++ sockets/challonge.js
@@ -8,6 +8,8 @@ module.exports = function(io) {
     var challongeHash = null;
     var matches = [];
     var players = [];
+    //Used to check for updates
+    var availableMatchesCache = [];
 
     var challongeIO = io.of('/challonge');
 
@@ -22,34 +24,34 @@ module.exports = function(io) {
         });
 
         socket.on('update challonge', function(msg) {
+            var urlChanged = msg.challongeUrl != challongeData.challongeUrl;
             challongeData = msg;
             if (challongeData.challongeUrl) {
-                if (!challongeHash) {
+                if (!challongeHash || urlChanged) {
                     challongeHash = createChallongeHash(challongeData.challongeUrl);
-                }
-
-                getchallongePollableData(challongeData);
-                setTimeout(pollChallonge, challongePollFrequency);
+                    pollChallonge();
+                }   
             }
 
-            challongeIO.emit('update challonge', challongeData);
+            //challongeIO.emit('update challonge', challongeData);
             console.log('update challonge: ' + JSON.stringify(challongeData));
         });
 
         function pollChallonge() {
-            getchallongePollableData(challongeData);
+            getChallongePollableData(challongeData);
             if (connectedSockets > 0 && challongeHash)
               setTimeout(pollChallonge, challongePollFrequency);
         }
 
-        function getchallongePollableData(challongeData) {
+        function getChallongePollableData(challongeData) {
             console.log('Polling challonge for updates...');
             challongeData = fetchChallongeData(challongeData);
             challongeData.upcomingMatches = getUpcomingMatches();
+            challongeData = checkForMatchUpdates(challongeData);
             challongeData.players = getPlayerDictionary();
             //console.log('New challonge Pollable Data: ' + JSON.stringify(challongeData));
             console.log('Challonge update complete');
-            socket.emit('update challonge', challongeData);
+            challongeIO.emit('update challonge', challongeData);
         }
 
         function fetchChallongeData(challongeData) {
@@ -57,7 +59,7 @@ module.exports = function(io) {
             var xmlhttp = new XMLHttpRequest();
             xmlhttp.open('GET', requestUrl, false);
             //Basic Auth must be encoded or request will be denied
-            xmlhttp.setRequestHeader("Authorization", "Basic " + new Buffer('Camtendo' + ':' + config.challongeApiKey).toString('base64')); 
+            xmlhttp.setRequestHeader("Authorization", "Basic " + new Buffer(config.challongeDevUsername + ':' + config.challongeApiKey).toString('base64')); 
             xmlhttp.send();
             console.log('Requesting tournament data for ' + challongeData.challongeUrl);
             var challongeResponse = JSON.parse(xmlhttp.responseText);
@@ -102,6 +104,56 @@ module.exports = function(io) {
             //Standard tournament
             challongeHash = tourneyHash;
             return challongeHash;
+        }
+
+        function checkForMatchUpdates(challongeData) {
+            if (Object.equals(challongeData.upcomingMatches, availableMatchesCache)) {
+                challongeData.matchesChanged = false;
+            }
+            else {
+                challongeData.matchesChanged = true;
+                availableMatchesCache = challongeData.upcomingMatches;
+                console.log('Matches changed. Will alert clients.');
+            }
+
+            return challongeData;
+        }
+
+        //Used to detect match changes with high accuracy
+        //https://stackoverflow.com/questions/1068834/object-comparison-in-javascript
+        Object.equals = function( x, y ) {
+          if ( x === y ) return true;
+            // if both x and y are null or undefined and exactly the same
+
+          if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
+            // if they are not strictly equal, they both need to be Objects
+
+          if ( x.constructor !== y.constructor ) return false;
+            // they must have the exact same prototype chain, the closest we can do is
+            // test there constructor.
+
+          for ( var p in x ) {
+            if ( ! x.hasOwnProperty( p ) ) continue;
+              // other properties were tested using x.constructor === y.constructor
+
+            if ( ! y.hasOwnProperty( p ) ) return false;
+              // allows to compare x[ p ] and y[ p ] when set to undefined
+
+            if ( x[ p ] === y[ p ] ) continue;
+              // if they have the same strict value or identity then they are equal
+
+            if ( typeof( x[ p ] ) !== "object" ) return false;
+              // Numbers, Strings, Functions, Booleans must be strictly equal
+
+            if ( ! Object.equals( x[ p ],  y[ p ] ) ) return false;
+              // Objects and Arrays must be tested recursively
+          }
+
+          for ( p in y ) {
+            if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+              // allows x[ p ] to be set to undefined
+          }
+          return true;
         }
     });
 }
