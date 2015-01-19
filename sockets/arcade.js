aec0b98d545e29f0c1927126ad9281df31a7ec7f
Cameron Crockrom <ccrockrom@gmail.com>
2015-01-19 15:06:48 -0600

    Finito.

---
+++ sockets/arcade.js
@@ -0,0 +1,23 @@
+module.exports = function (io) {
+  var data = [];
+
+  var arcade = io.of('/arcade');
+
+  arcade.on('connection', function(socket) {
+    // Log the new connection
+    console.log('velvet arcade user connected: ' + socket.handshake.address + ' -> ' + socket.request.headers.referer);
+
+    socket.emit('update arcade', data);
+
+    socket.on('disconnect', function() {
+      console.log('velvet arcade user disconnected: ' + socket.handshake.address);
+    });
+
+    socket.on('update arcade', function (msg) {
+      data = msg;
+      console.log('update arcade: ' + JSON.stringify(data));
+      arcade.emit('update arcade', data);
+    });
+
+  });
+}
