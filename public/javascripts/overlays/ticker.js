03bf62e130e18532f43dc888b96f40d9804f8155
Noah Swartz <nswartz1990@gmail.com>
2015-05-03 02:23:54 -0500

    Basic results scrolling.

--- public/javascripts/overlays/ticker.js
+++ public/javascripts/overlays/ticker.js
@@ -1,6 +1,4 @@
-var TweenMax = require('/../../gsap');
-
-var data = {
+var tickerData = {
   game: 'Melee',
   results: [
     {
@@ -14,47 +12,75 @@ var data = {
       loser: 'MegaRobMan',
       wScore: 3,
       lScore: 1
+    },
+    {
+      winner: 'Majik',
+      loser: 'Cue',
+      wScore: 3,
+      lScore: 2
+    },
+    {
+      winner: 'Bee',
+      loser: 'Doodler',
+      wScore: 3,
+      lScore: 1
+    },
+    {
+      winner: 'Tyser',
+      loser: 'Darkrain',
+      wScore: 3,
+      lScore: 0
+    },
+    {
+      winner: 'Smeesh',
+      loser: 'RFrizzle',
+      wScore: 3,
+      lScore: 1
     }
-  ]
+  ],
+  announcement: "We will be taking an hour break for lunch at 1:00 PM CDT. We will return promptly at 2:00. Thank you for your understanding!"
 };
 
-var ticker = {};
+var mainTimeline = new TimelineMax({ paused: true, repeat: -1 });
+
+function go() {
+  updateResults();
+  mainTimeline.resume();
+}
 
 function updateResults() {
-  var tl = new TimelineMax(paused: true);
-  var resultList = document.getElementById('resultList');
+  var tl = new TimelineMax();
+  var resultList = document.getElementById('ticker-display');
   resultList.innerHTML = '';
 
   var speed = 50;
-  data.results.forEach(function(o) {
+  tickerData.results.forEach(function(o) {
     var e = createResult(o);
-    var width = e.offsetWidth;
     resultList.appendChild(e);
+    var width = e.offsetWidth;
 
     tl.to(resultList, 
-      width/speed, 
-      { marginLeft: -width, ease: Linear.easeNone }, 
-      onComplete: cycleResult, 
-      onCompleteParams: [e, resultList]);
+          width/speed, 
+          { marginLeft: '-=' + width.toString() + 'px', 
+            ease: Linear.easeNone, 
+            onComplete: cycleResult, 
+            onCompleteParams: [e, resultList] 
+          })
+      .set(resultList, { marginLeft: 0 });
   });
+  
+  mainTimeline.add(tl);
+}
+
+function createResult(o) {
+  var e = document.createElement('div');
+  e.innerText = o.winner + ' ' + o.wScore + '-' + o.lScore + ' ' + o.loser;
+  e.className = 'tickerResult tickerText';
+
+  return e;
 }
 
 function cycleResult(element, parent) {
   parent.appendChild(element);
 }
 
-function playResults() {
-
-}
-
-function createScoreTimeline(el) {
-
-}
-
-function createResult(o) {
-  var e = document.createElement('div');
-  e.innerText = o.winner + ' ' + o.wScore + '-' + o.lScore + ' ' + o.loser;
-  e.className = 'tickerScore';
-
-  return e;
-}
